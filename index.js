bf4e52d0e6fc401fa6b649b1fe39de5eaf10a8de
yashp241195 <yashp241195@gmail.com>
2021-10-01 18:00:43 +0530

    deploy on kubernetes

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const PORT = 3010
 
 app.get('/',(req,res)=>{res.json({H:"Welcome to app-1"})})
 
-app.listen(PORT,()=>{console.log('Welcome to app-1')})
+app.listen(PORT, ()=>{ console.log('Welcome to app-1') })
